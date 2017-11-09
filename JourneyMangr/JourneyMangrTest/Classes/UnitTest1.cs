35c233ed130c813fe125f3c61f290d419d8923aa
Major Márk <mmarky188@gmail.com>
2017-11-09 17:15:39 +0100

    preparation for tests

---
+++ JourneyMangr/JourneyMangrTest/Classes/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JourneyMangr;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JourneyMangr.Tests
+{
+    [TestClass()]
+    public class UnitTest1
+    {
+        [TestMethod()]
+        public void GetCarDataListTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CalcFTperKMTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteCarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetCarListTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAutoIDTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetCarDataTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddCarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddCarDataTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
